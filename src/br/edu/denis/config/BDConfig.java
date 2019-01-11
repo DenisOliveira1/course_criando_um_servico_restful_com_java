bf1588ddf982fa2fdc05760f609a112ddb9d5e75
Denis Oliveira <denisb.oliveira@live.com>
2019-01-10 23:42:05 -0200

    c3 - Remove conflito com a possibilidade de usar o servidor glassfish.

--- src/br/edu/denis/config/BDConfig.java
+++ src/br/edu/denis/config/BDConfig.java
@@ -12,7 +12,7 @@ public class BDConfig {
 		String pass = "root";	
 		String tabela = "notes_db";
 		String url = "jdbc:mysql://localhost:3306/";
-		String fix = "?useTimezone=true&serverTimezone=UTC";
+		String fix = "?useTimezone=true&serverTimezone=UTC&useSSL=false";
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");//o driver trabalha com qualquer banco de dados.Essa linha é uma garantia de que o drivermanager reconhecera que o mysql que sera usado
 		return DriverManager.getConnection(url+tabela+fix,user,pass);//cria uma conexao e retorna ela
